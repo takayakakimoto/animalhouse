84a78dd6cb5ec871ba1cbe4014ad1f2b94775916
kanchi.nagao <wanzhiyongwei@gmail.com>
2019-01-30 13:27:25 +0000

    post

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,8 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @post = current_user.posts.build  # form_for 用
+      @posts = current_user.posts.order('created_at DESC').page(params[:page])
+    end
   end
 end
