78b2996d5d495bc4ea227d41341b7558b66671ef
kanchi.nagao <wanzhiyongwei@gmail.com>
2019-01-31 13:34:48 +0000

    post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,14 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   
   validates :content, presence: true, length: { maximum: 255 }
+  validate  :image_size
+  
+   private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def image_size
+      if image.size > 5.megabytes
+        errors.add(:image, "should be less than 5MB")
+      end
+    end
 end
